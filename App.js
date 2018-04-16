9801f263c9a2f0c5ac97ef7229f69d79531ef2e8
PTMBR033 <skim@pasonatquila.com>
2018-04-16 17:18:01 +0900

    Input and To Do Components

--- App.js
+++ App.js
@@ -1,22 +1,50 @@
 import React from 'react';
-import { StyleSheet, Text, View, StatusBar, TextInput, Dimensions, Platform } from 'react-native';
-
+import { 
+  StyleSheet, 
+  Text, 
+  View, 
+  StatusBar, 
+  TextInput, 
+  Dimensions, 
+  Platform, 
+  ScrollView 
+} from 'react-native';
+import ToDo from './ToDo'
 const {height, width } = Dimensions.get("window");
 
 export default class App extends React.Component {
+  state = {
+    newToDo: ""
+  }
   render() {
+    const { newToDo } = this.state;
     return (
       <View style={styles.container}>
         <StatusBar barStyle="light-content" />
         <Text style={styles.title}>Kawai To Do</Text>
         <View style={styles.card}>
-          <TextInput style={styles.input} placeholder={"New To Do"} />
+          <TextInput 
+            style={styles.input} 
+            placeholder={"New To Do"} 
+            value={newToDo} 
+            onChangeText={this._controlNewToDo}
+            placeholderTextColor={"#999"} 
+            returnKeyType={"done"} 
+            autoCorrect={false} />
+          <ScrollView>
+            <ToDo />
+          </ScrollView>
         </View>
       </View>
     );
   }
 }
 
+_controlNewToDo = text => {
+  this.setState({
+    newToDo: text
+  })
+}
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -48,5 +76,10 @@ const styles = StyleSheet.create({
       }
     })
   },
-  input: {}
+  input: {
+    padding: 20,
+    borderBottomColor: "#bbb",
+    borderBottomWidth: 1,
+    fontSize: 25
+  }
 });
